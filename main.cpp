cb7d2011447cb7f8f407778bcf53ce9914f9fccd
xyro10 <sobhanvaez10@gmail.com>
2025-10-01 15:47:32 +0330

    add function declarations for random number generation

--- main.cpp
+++ main.cpp
@@ -10,3 +10,5 @@ void moveLeft(int game[4][4]);
 void moveRight(int game[4][4]);
 void moveUp(int game[4][4]);
 void moveDown(int game[4][4]);
+void randFirstNum(int game[4][4]);
+void randContinueNum(int game[4][4]);
