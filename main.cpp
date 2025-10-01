7609e325de7246e4d059ae59395b8bc414bb3c89
xyro10 <sobhanvaez10@gmail.com>
2025-10-01 15:59:07 +0330

    complete jadval function to display the game board

--- main.cpp
+++ main.cpp
@@ -13,3 +13,25 @@ void moveDown(int game[4][4]);
 void randFirstNum(int game[4][4]);
 void randContinueNum(int game[4][4]);
 bool isWinLose(int game[4][4]);
+
+
+void jadval(int game[4][4]) {    
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << "-------";
+        }
+        cout << "-" << endl;
+        for (int j = 0; j < 4; j++)
+        {
+            cout << "| " << setw(4) << game[i][j] << " ";
+        }
+        cout << "|" << endl;
+        for (int j = 0; j < 4; j++)
+        {
+            cout << "-------";
+        }
+        cout << "-" << endl;
+    }
+}
